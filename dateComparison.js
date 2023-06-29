5b93299df2ac3eee74d5f4252d4ad327bb6eb7a6
papadog218 <mki218@naver.com>
2023-06-30 00:42:26 +0900

    date comparison 풀이 제한사항 적용1

--- dateComparison.js
+++ dateComparison.js
@@ -1,7 +1,40 @@
 function solution(date1, date2) {
-    var answer = 0;
+    let answer = 0;
+
+    console.log(date1[0])
+    console.log(typeof date1[0])
+    console.log(0 <= date1[0] >= 10000)
+    console.log(!(0 <= date1[0] >= 10000))
+    
+    console.log(date2[0])
+    console.log(typeof date2[0])
+    console.log(0 <= date2[0] <= 10000)
+    console.log(!(0 <= date2[0] <= 10000))
+
+    if (!(0 <= date1[0] <= 10000)) {
+        alert("date1 year error");
+        return;
+    } else if (!(0 <= date2[0] <= 10000)) {
+        alert("date2 year error");
+        return;
+    }
+
     let convertDate1 = new Date(date1[0], date1[1]-1, date1[2]);
     let convertDate2 = new Date(date2[0], date2[1]-1, date2[2]);
+
+    if (!(1 <= date1[1] <= 12)) {
+        alert("date1 month error");
+        return;
+    } else if (!(1 <= date2[1] <= 12)) {
+        alert("date2 month error");
+        return;
+    }
+
+    // if (1 <= date1[2] <= ) {
+
+    // }
+
     convertDate1 < convertDate2 && (answer = 1);
+
     return answer;
 }
