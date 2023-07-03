9ff1bfb55268ab2053d4975f8b28704c79939ec4
용환 김 <yhKim@duranno.com>
2023-07-03 18:25:01 +0900

    로그 정리 _김용환

--- diceGame.js
+++ diceGame.js
@@ -2,15 +2,18 @@ function solution(a, b) {
     var answer = 0;
     // a와 b가 모두 홀수라면 a2 + b2 점을 얻습니다.
     if (a % 2 != 0 & b % 2 != 0) {
-        alert("모두 홀수!");
+        console.log("모두 홀수!");
+        console.log("(a**2) + (b**2) = ");
         answer = (a**2) + (b**2);
     } else if (a % 2 != 0 | b % 2 != 0) { // a와 b 중 하나만 홀수라면 2 × (a + b) 점을 얻습니다.
-        alert("하나만 홀수!");
+        console.log("하나만 홀수!");
+        console.log("2*(a+b) = ");
         answer = 2*(a+b);
     }
     // a와 b 모두 홀수가 아니라면 |a - b| 점을 얻습니다.
     if (a % 2 == 0 && b % 2 == 0) {
-        alert("모두 짝수!");
+        aconsole.logert("모두 짝수!");
+        console.log("|a-b| = ");
         answer = Math.abs(a-b);
     }
     return answer;
